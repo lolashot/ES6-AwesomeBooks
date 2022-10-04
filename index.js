4b0b51538bd7cb7961bb410191ab741a6f2b713d
lolashot <ololadeshot@gmail.com>
2022-10-04 11:40:25 +0100

    linter corrections

--- index.js
+++ index.js
@@ -1,72 +1,67 @@
-import AllBooks from "./modules/AllBooks.js"
+import AllBooks from './modules/AllBooks.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    AllBooks.displayBooks();
-  });
+  AllBooks.displayBooks();
+});
 
-  
-  const removeBook = (id) => {
-    console.log("hhh", "jjj");
-    const bookData = AllBooks.getBooks();
-    const filteredBooks = bookData.filter((item) => item.id !== id);
-    AllBooks.localStorage('books', filteredBooks);
-  };
-  
-  window.removeBook = removeBook;
+const removeBook = (id) => {
+  const bookData = AllBooks.getBooks();
+  const filteredBooks = bookData.filter((item) => item.id !== id);
+  AllBooks.localStorage('books', filteredBooks);
+};
+
+window.removeBook = removeBook;
 
 document.getElementById('add-book-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-  
-    const bookId = AllBooks.getBook();
-    const bookObject = new AllBooks(
-      bookId,
-      document.getElementById('title').value,
-      document.getElementById('author').value,
-    );
-    AllBooks.addBook(bookObject);
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
+  e.preventDefault();
+
+  const bookId = AllBooks.getBook();
+  const bookObject = new AllBooks(
+    bookId,
+    document.getElementById('submit').value,
+    document.getElementById('submit').value
+  );
+  AllBooks.addBook(bookObject);
+  document.getElementById('submit').value = '';
+  document.getElementById('submit').value = '';
+});
+
+const navItems = document.getElementsByClassName('nav-item');
+for (let i = 0; i < navItems.length; i += 1) {
+  navItems[i].addEventListener('click', function navElements() {
+    const current = document.getElementsByClassName('active');
+    current[0].className = current[0]?.className?.replace('active', '');
+    this.className += 'active';
   });
-  
-  const navItems = document.getElementsByClassName('nav-item');
-  for (let i = 0; i < navItems.length; i += 1) {
-    navItems[i].addEventListener('click', function navElements() {
-      const current = document.getElementsByClassName('active');
-      current[0].className = current[0]?.className?.replace(' active', '');
-      this.className += ' active';
-    });
-  }
-  
-  const listSelected = () => {
-    document.getElementById('add-books').style.display = 'none';
-    document.getElementById('contact').style.display = 'none';
-    document.getElementById('books-list').style.display = 'block';
-  }
-  
-  const addBooksSelected = () => {
-    document.getElementById('books-list').style.display = 'none';
-    document.getElementById('contact').style.display = 'none';
-    document.getElementById('add-books').style.display = 'flex';
-  }
-  
-  const contactSelected = () => {
-    document.getElementById('books-list').style.display = 'none';
-    document.getElementById('add-books').style.display = 'none';
-    document.getElementById('contact').style.display = 'block';
-  }
-  
-  const navList = document.querySelector('.list-nav');
-  const navAdd = document.querySelector('.add-book-nav');
-  const navContact = document.querySelector('.contact-nav');
-  
-  navList.addEventListener('click', listSelected);
-  navAdd.addEventListener('click', addBooksSelected);
-  navContact.addEventListener('click', contactSelected);
-  
+}
 
+const listSelected = () => {
+  document.getElementById('add-books').style.display = "none";
+  document.getElementById('contact').style.display = "none";
+  document.getElementById("books-list").style.display = "block";
+};
 
+const addBooksSelected = () => {
+  document.getElementById('books-list').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+  document.getElementById('add-books').style.display = 'flex';
+};
 
-  const time = document.querySelector('.time');
+const contactSelected = () => {
+  document.getElementById('books-list').style.display = 'none';
+  document.getElementById('add-books').style.display = 'none';
+  document.getElementById('contact').style.display = 'block';
+};
+
+const navList = document.querySelector('.list-nav');
+const navAdd = document.querySelector('.add-book-nav');
+const navContact = document.querySelector('.contact-nav');
+
+navList.addEventListener('click', listSelected);
+navAdd.addEventListener('click', addBooksSelected);
+navContact.addEventListener('click', contactSelected);
+
+const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 
 let formatTime = (date) => {
@@ -84,9 +79,9 @@ let formatTime = (date) => {
   return `${hour.toString().padStart(2, '0')}:${minutes
     .toString()
     .padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${state}`;
-}
+};
 
- let formatDate = (date) => {
+let formatDate = (date) => {
   const months = [
     'Jan',
     'Feb',
@@ -120,7 +115,7 @@ let formatTime = (date) => {
   }
 
   return `${months[month]} ${day}${state} ${year},`;
-}
+};
 
 setInterval(() => {
   const today = new Date();
